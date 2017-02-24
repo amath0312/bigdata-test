425e3ae2df955ad39402f141924889c2e9c660c3
amath0312 <amath0312@gmail.com>
2017-02-24 23:01:20 +0800

    get address of ip

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 # -*- coding:utf8 -*-
 
 from flask import Flask, render_template, url_for, request,redirect,make_response,session
+import BaiduMapLocation
+import JCloudGPS
 
 app = Flask(__name__)
 @app.route('/')
 def index():
     client_ip = request.remote_addr
-    lat,lng = JCloudGPS().get_gps(client_ip)
-    return render_template('index.html', info=client_ip, address=lat+','+lng)
+    lat,lng = JCloudGPS.Requester().get_gps(client_ip)
+    address = BaiduMapLocation.Requester().get_location(lat,lng)
+    return render_template('index.html', info=client_ip, address=lat+','+lng+': '+address)
 
 
 if __name__ == '__main__':
